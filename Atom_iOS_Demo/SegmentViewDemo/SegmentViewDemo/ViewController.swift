94c828e0e206b09a386c6ea75b4a9ed770b39343
雷地 <liuling@liulingdeMacBook-Pro.local>
2017-05-12 16:41:16 +0800

    SegmentView
    SegmentView

---
+++ Atom_iOS_Demo/SegmentViewDemo/SegmentViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegmentViewDemo
+//
+//  Created by 刘岭 on 17/5/12.
+//  Copyright © 2017年 刘岭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
